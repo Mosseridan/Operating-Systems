f60e45df53a963f7888ec139f3688ffc7f61a5fb
Guy Hecht <guyhecht1990@gmail.com>
2017-06-21 17:46:05 +0300

    only sanity left

---
+++ lsof.c
@@ -0,0 +1,116 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
+
+struct status{
+  ushort state;
+  uint size;
+};
+
+struct fd {
+  uint type;
+  int ref; // reference count
+  char readable;
+  char writable;
+  uint inum;
+  uint off;
+};
+
+struct blockstat {
+  uint total_blocks;
+  uint free_blocks;
+  uint num_of_access;
+  uint num_of_hits;
+};
+
+struct inodestat {
+  ushort total;
+  ushort free;
+  ushort valid;
+  uint refs;
+  ushort used;
+};
+
+// char*
+// fmtname(char *path)
+// {
+//   static char buf[DIRSIZ+1];
+//   char *p;
+//
+//   // Find first character after last slash.
+//   for(p=path+strlen(path); p >= path && *p != '/'; p--)
+//     ;
+//   p++;
+//
+//   // Return blank-padded name.
+//   if(strlen(p) >= DIRSIZ)
+//     return p;
+//   memmove(buf, p, strlen(p));
+//   memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+//   return buf;
+// }
+//
+// void
+// ls(char *path)
+// {
+//   char buf[512], *p;
+//   int fd;
+//   struct dirent de;
+//   struct stat st;
+//
+//   if((fd = open(path, 0)) < 0){
+//     printf(2, "ls: cannot open %s\n", path);
+//     return;
+//   }
+//
+//   if(fstat(fd, &st) < 0){
+//     printf(2, "ls: cannot stat %s\n", path);
+//     close(fd);
+//     return;
+//   }
+//
+//   switch(st.type){
+//   case T_FILE:
+//     printf(1, "%s %d %d %d\n", fmtname(path), st.type, st.ino, st.size);
+//     break;
+//
+//   case T_DEV: //This line was added to make ls.c treat "proc" as a directory, and, as an unfortunate by-product, other devices aswell. (FROM THE ASSIGNMENT FAQ)
+//   case T_DIR:
+//     if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+//       printf(1, "ls: path too long\n");
+//       break;
+//     }
+//     strcpy(buf, path);
+//     p = buf+strlen(buf);
+//     *p++ = '/';
+//     while(read(fd, &de, sizeof(de)) == sizeof(de)){
+//       if(de.inum == 0)
+//         continue;
+//       memmove(p, de.name, DIRSIZ);
+//       p[DIRSIZ] = 0;
+//       if(stat(buf, &st) < 0){
+//         printf(1, "ls: cannot stat %s\n", buf);
+//         continue;
+//       }
+//       printf(1, "%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+//     }
+//     break;
+//   }
+//   close(fd);
+// }
+
+int
+main(int argc, char *argv[])
+{
+  printf(1,"\nlsof is starting...\n\n");
+  // int i;
+  //
+  // if(argc < 2){
+  //   ls(".");
+  //   exit();
+  // }
+  // for(i=1; i<argc; i++)
+  //   ls(argv[i]);
+  exit();
+}
